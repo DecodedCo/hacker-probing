29cb73daa892e526e4e062283aab70e99d442fd7
Tom Wilshere <tomw@decoded.com>
2015-08-18 15:56:52 +0100

    Added Ip whitelist script and example db connection.

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+/*****************************\
+| DO NOT REMOVE THIS FUNCTION |
+\*****************************/
+app.use(function(request, response, next) {
+  var ipAddr = request.headers["x-forwarded-for"];
+  if (ipAddr){
+    var list = ipAddr.split(",");
+    ipAddr = list[list.length-1];
+  } else {
+    ipAddr = request.connection.remoteAddress;
+  }
+  // Allow access to labs
+  allowed_ips = ['::1','127.0.0.1','localhost','::ffff:127.0.0.1'];
+  if (ipAddr === process.env.LABS_IP || allowed_ips.indexOf(ipAddr) != -1) {
+    next();
+  } else {
+    response.status(403);
+    response.send('Direct access forbidden');
+    response.end();
+  }
+})
+/*****************\
+| OK, CARRY ON... |
+\*****************/
+
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
