c2a266dfa62c257401734b629989598d2ec6861c
Tom Wilshere <tomw@decoded.com>
2015-08-20 11:12:56 +0100

    Added example DB call.

--- index.js
+++ index.js
@@ -38,6 +38,20 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+var pg = require('pg');
+
+app.get('/db', function(request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
