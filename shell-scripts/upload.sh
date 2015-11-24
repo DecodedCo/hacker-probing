e6e6476c87277ad9c47dc0dbef27517706dd7996
Amadeus Stevenson <amadeus@decoded.us>
2015-11-24 17:27:22 -0500

    Add upload code

--- shell-scripts/upload.sh
+++ shell-scripts/upload.sh
@@ -2,5 +2,8 @@
 
 # Reads in MAC SSID from sniff.sh and prepares for upload
 
-./sniff.sh | sed -e 's/ /=/' -e 's/$/&/' \
-  | tr -d '\n'
+# Where the API sits
+server="amadeus:5000"
+
+./sniff.sh | sed -e 's/ /,/' > output.log
+curl -F data=@output.log $server
