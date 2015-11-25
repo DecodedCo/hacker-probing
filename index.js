c915f598d7d8dd4c3189d4bda1484b848a6c9750
Amadeus Stevenson <amadeus@decoded.us>
2015-11-25 11:50:34 -0500

    Add new endpoints

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // example curl:
-// curl -F data=@output.log ip:5000
+// curl -F data=@output.log ip:5000/api/collect/
 
 var express = require('express');
 var app = express();
@@ -35,28 +35,45 @@ app.use(function(request, response, next) {
 | OK, CARRY ON... |
 \*****************/
 
-app.get('/api/', function(req, res) {
+// Return all ssid counts
+app.get('/api/ssids/', function(req, res) {
   fs.readFile('ssids.json', function (err, data) {
     if (err) throw err;
     res.json(JSON.parse(data));
   });
 });
 
-// make this a get request so it's easy to hit via a browser, rather than the better DELETE request
-app.get('/delete/', function(req, res) {
-  fs.writeFile('ssids.json', "{}", function(err) {
-        if (err) throw err;
-        console.log("Emptied ssid file");
-        res.end("Deleted\n");
+// Return all users and their ssids for network analysis
+app.get('/api/users/', function(req, res) {
+  fs.readFile('users.json', function (err, data) {
+    if (err) throw err;
+    res.json(JSON.parse(data));
   });
 });
 
-app.post('/', upload.single('data'), function(req, res) {
+// Clear out data
+// make this a get request so it's easy to hit via a browser, rather than the better DELETE request
+app.get('/api/delete/', function(req, res) {
+  fs.writeFile('ssids.json', "{}", function(err) {
+    if (err) throw err;
+    console.log("Emptied ssid file");
+    fs.writeFile('users.json', "{}", function(err) {
+      if (err) throw err;
+      console.log("Emptied users file");
+      res.json({"message" : "Cleared"});
+    });
+  });
+});
+
+// Process incoming data and store
+app.post('/api/collect/', upload.single('data'), function(req, res) {
+  // convert file upload to string
   var incomingData = req.file.buffer.toString();
-  // get fresh copy of db
+  // get fresh copy of current stored ssids
   fs.readFile('ssids.json', function (err, data) {
     if (err) throw err;
     var ssids = JSON.parse(data);
+    // convert uploaded csv data to an object
     var converter = new Converter({
       noheader: true,
       headers: ["MAC", "SSID"]
@@ -64,7 +81,7 @@ app.post('/', upload.single('data'), function(req, res) {
     converter.fromString(incomingData, function(err,parsedData){
       if (err) throw err;
       console.log("Received ",parsedData.length, " items");
-      // MVP: just tally list of SSIDs by number
+      // tally list of SSIDs by number
       parsedData.forEach(function(ssid) {
         if (ssid.SSID in ssids) {
           ssids[ssid.SSID] += 1;
@@ -73,20 +90,54 @@ app.post('/', upload.single('data'), function(req, res) {
         }
       });
 
+      // save the result
       fs.writeFile('ssids.json', JSON.stringify(ssids), function(err) {
         if (err) throw err;
-        console.log("Saved to disk");
-        res.end("Om nom nom\n");
+        console.log("Saved ssids to disk");
+
+        // now load up users.json and update
+        fs.readFile('users.json', function (err, data) {
+          if (err) throw err;
+          var users = JSON.parse(data);
+          var converter = new Converter({
+            noheader: true,
+            headers: ["MAC", "SSID"]
+          });
+          converter.fromString(incomingData, function(err,parsedData){
+            if (err) throw err;
+            // compile a list of ssids by user for network analysis
+            parsedData.forEach(function(user) {
+              if (user.MAC in users) {
+                if (users[user.MAC].indexOf(user.SSID) > -1) {
+                  // skip as SSID already noted
+                } else {
+                  // add new mac to this user
+                  users[user.MAC].push(user.SSID);
+                }
+              } else {
+                // create the user with their first ssid
+                users[user.MAC] = Array(user.SSID);
+              }
+            });
+            // save the data
+            fs.writeFile('users.json', JSON.stringify(users), function(err) {
+              if (err) throw err;
+              console.log("Saved users to disk");
+              res.end("Om nom nom");
+            });
+          });
+        });
       });
-
     });
-
   });
-
 });
 
+// Feedback on available endpoints
+app.get('/api/', function(req, res) {
+  res.json( { "Availabile endpoints": ["GET /api/ssids/", "GET /api/users/", "GET /api/delete/", "POST /api/collect/"] } );
+});
+
+// Switch it on
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
