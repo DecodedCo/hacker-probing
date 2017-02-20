186a6884e956df64c063b9e7f08427ed003a6981
tomwilshere <tom.wilshere@gmail.com>
2017-02-20 16:48:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,31 +10,6 @@ var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000));
 
-/*****************************\
-| DO NOT REMOVE THIS FUNCTION |
-\*****************************/
-app.use(function(request, response, next) {
-  var ipAddr = request.headers["x-forwarded-for"];
-  if (ipAddr){
-    var list = ipAddr.split(",");
-    ipAddr = list[list.length-1];
-  } else {
-    ipAddr = request.connection.remoteAddress;
-  }
-  // Allow access to labs
-  allowed_ips = ['::1','127.0.0.1','localhost','::ffff:127.0.0.1','::ffff:172.16.1.201'];
-  if (ipAddr === process.env.LABS_IP || allowed_ips.indexOf(ipAddr) != -1) {
-    next();
-  } else {
-    response.status(403);
-    response.send('Direct access forbidden');
-    response.end();
-  }
-})
-/*****************\
-| OK, CARRY ON... |
-\*****************/
-
 // Return all ssid counts
 app.get('/api/ssids/', function(req, res) {
   fs.readFile('ssids.json', function (err, data) {
